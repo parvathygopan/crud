d802d3e63c97b9a7cd9e7481105fe7c6a3a70bba
parvathygopan <parvathy671999@gmail.com>
2023-03-11 13:17:23 +0530

    crud op

---
+++ src/Firebase/config.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+const firebaseConfig = {
+    apiKey: "AIzaSyBzhShoaeuoosk46yaS3h1bdOQet4wwVsk",
+    authDomain: "storehere-35abc.firebaseapp.com",
+    projectId: "storehere-35abc",
+    storageBucket: "storehere-35abc.appspot.com",
+    messagingSenderId: "1068177031670",
+    appId: "1:1068177031670:web:29f1bfbd4035c6c4c307d7",
+    measurementId: "G-FJ4H70147D"
+  };
+  export const Firebase = firebase.initializeApp(firebaseConfig);
