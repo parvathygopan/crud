bca6f0157947c57a3172d266a7abc554e4169251
parvathygopan <parvathy671999@gmail.com>
2023-03-11 15:22:40 +0530

    import chng

--- src/Service/config.js
+++ src/Service/config.js
@@ -1,5 +1,5 @@
 // import firebase from "firebase";
-import  initializeApp from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 const firebaseConfig = {
     apiKey: "AIzaSyBzhShoaeuoosk46yaS3h1bdOQet4wwVsk",
     authDomain: "storehere-35abc.firebaseapp.com",
@@ -9,6 +9,5 @@ const firebaseConfig = {
     appId: "1:1068177031670:web:29f1bfbd4035c6c4c307d7",
     measurementId: "G-FJ4H70147D"
   };
-  // export const Firebase = firebase.initializeApp(firebaseConfig);
   const app = initializeApp(firebaseConfig);
-  export default app = initializeApp;
+  export default app = initializeApp(firebaseConfig);
