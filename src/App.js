c1082e796da464ceb8fc90ab1b7d0a8fe344091c
parvathygopan <parvathy671999@gmail.com>
2023-03-11 15:13:24 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import { Firebase } from './Service/config';
+import app from "./Service/config";
 
 function App() {
   return (
     <div className="App">
    <button onClick={() => {
-    Firebase.firestore().collection('students').get().then(snapshot => {
+    app.firestore().collection('students').get().then(snapshot => {
       snapshot.forEach((obj)=>{
         console.log(obj.data())
       })
     })
    }}>clickhere</button>
    <button onClick={() => {
-    Firebase.firestore().collection('students').doc('32oFcY5mF9b1FkGA0jUp').delete().then(() => {
+    app.firestore().collection('students').doc('32oFcY5mF9b1FkGA0jUp').delete().then(() => {
       console.log("deleted successfully")
     })
    }}>add</button>
